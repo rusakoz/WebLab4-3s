e0261c942607eeefd098962e2d3566d44d3dd7ae
Ruslan <julyrak@gmail.com>
2023-12-19 00:42:57 +0300

    First Commit

---
+++ src/test/java/org/lab4/wed/weblab4/WebLab4ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lab4.wed.weblab4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebLab4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
