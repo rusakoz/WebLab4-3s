ad2299dddb971f2946ada2dfb8a6424009523524
Ruslan <julyrak@gmail.com>
2023-12-21 02:44:25 +0300

    update 2.0.1

---
+++ src/main/resources/static/js/components/Clock.vue
@@ -0,0 +1,21 @@
+<script setup>
+import {onBeforeMount, onMounted, ref} from 'vue'
+let message = ref()
+
+onBeforeMount(function (){
+    setInterval(()=>{message.value = new Date().toLocaleDateString() + " " + new Date().toLocaleTimeString()}, 1000)
+})
+</script>
+
+<template>
+<div>
+    <button class="main-btn" id="main-btn" type="button">
+        <span class="link-text">Fffffff</span>
+        <span id="clock">{{ message }}</span>
+    </button>
+</div>
+</template>
+
+<style scoped>
+
+</style>
