b9c6c9ba85128e719a222e667593c9edfab4fc1c
Ruslan <julyrak@gmail.com>
2024-01-18 02:41:14 +0300

    upd 2.1

--- src/main/resources/js/components/Login.vue
+++ src/main/resources/js/components/Login.vue
@@ -1,31 +1,32 @@
 <template>
-    <div class="main-container">
-        <div class="container">
-            <div class="wrapper-container">
-                <div class="login-container">
-                  <!-- <pre>{{ form }}</pre> -->
-                    <small v-if="authErr" class="errors">{{ authErrText }}</small>
-                    <div class="form-control" :class="{invalid: !form.name.valid && form.name.touched}">
-                      <label>Имя</label>
-                      <input class="name" placeholder="Введите имя" v-model="form.name.value" @blur="form.name.blur">
-                      <small v-if="form.name.errors.required && form.name.touched" class="errors">Введите имя</small>
-                    </div>
-                    <div class="form-control" :class="{invalid: !form.password.valid && form.password.touched}">
-                      <label>Пароль</label>
-                      <input class="password" type="password" placeholder="Введите пароль" v-model="form.password.value" @blur="form.password.blur">
-                      <small v-if="form.password.errors.required && form.password.touched" class="errors">Введите пароль</small>
-                      <small v-else-if="form.password.errors.minLength && form.password.touched" class="errors">
-                        Символов введено {{ form.password.value.length }} из {{ minPassLength }}.
-                      </small>
-                    </div>
-                    <button @click="submit" class="btn-auth" :disabled="!form.valid">Войти</button>
-                    <h6 class="text-reg">Нет аккаунта? 
-                        <router-link to="/registration" class="link-reg">Регистрация</router-link>
-                    </h6>
-                </div>
-            </div>
+  <div class="main-container">
+    <div class="container">
+      <div class="wrapper-container">
+        <div class="login-container">
+          <!-- <pre>{{ form }}</pre> -->
+          <small v-if="authErr" class="errors">{{ authErrText }}</small>
+          <div class="form-control" :class="{ invalid: !form.name.valid && form.name.touched }">
+            <label>Имя</label>
+            <input class="name" placeholder="Введите имя" v-model="form.name.value" @blur="form.name.blur">
+            <small v-if="form.name.errors.required && form.name.touched" class="errors">Введите имя</small>
+          </div>
+          <div class="form-control" :class="{ invalid: !form.password.valid && form.password.touched }">
+            <label>Пароль</label>
+            <input class="password" type="password" placeholder="Введите пароль" v-model="form.password.value"
+              @blur="form.password.blur">
+            <small v-if="form.password.errors.required && form.password.touched" class="errors">Введите пароль</small>
+            <small v-else-if="form.password.errors.minLength && form.password.touched" class="errors">
+              Символов введено {{ form.password.value.length }} из {{ minPassLength }}.
+            </small>
+          </div>
+          <button @click="submit" class="btn-auth" :disabled="!form.valid">Войти</button>
+          <h6 class="text-reg">Нет аккаунта?
+            <router-link to="/registration" class="link-reg">Регистрация</router-link>
+          </h6>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script setup>
@@ -48,150 +49,127 @@ let authErrText = ref('')
 const form = useForm({
   name: {
     value: '',
-    validators: {required}
+    validators: { required }
   },
   password: {
     value: '',
-    validators: {required, minLength: minLength(minPassLength)}
+    validators: { required, minLength: minLength(minPassLength) }
   }
 })
 
-async function submit(){
+async function submit() {
 
-    const response = await useFetchPost("/user/login", {userLogin: form.name.value, 
-                                                     userPassword: form.password.value
-                                                    }
-    ).catch(()=>{
-      authErrText.value = "Ошибка авторизации"
+  const response = await useFetchPost("/user/login", {
+    userLogin: form.name.value,
+    userPassword: form.password.value
+  }
+  ).catch(() => {
+    authErrText.value = "Ошибка авторизации"
+    authErr.value = true
+  })
+
+  if (response.status === 200) {
+    const resJson = response.json()
+
+    let errorText
+    await resJson.then((res) => { errorText = res.error })
+
+    if (typeof errorText !== 'undefined') {
+      authErrText.value = errorText
       authErr.value = true
-    })
-
-    if(response.status === 200){
-      const resJson = response.json()
-
-      let errorText
-      await resJson.then((res)=>{errorText = res.error})
-
-      if(typeof errorText !== 'undefined'){
-        authErrText.value = errorText
-        authErr.value = true
-      }else{
-        resJson.then((res)=>{
-            localStorage.setItem('userAccessToken', res.accessToken)
-            localStorage.setItem('userRefreshToken', res.refreshToken)
-            localStorage.setItem('isLoggin', true)
-            store.commit('setAuthValue', true)
-          })
-        router.replace('/hello')
-      }
-    }else{
-      authErrText.value = "Ошибка авторизации"
-      authErr.value = true
+    } else {
+      resJson.then((res) => {
+        localStorage.setItem('userAccessToken', res.accessToken)
+        localStorage.setItem('userRefreshToken', res.refreshToken)
+        localStorage.setItem('isLoggin', true)
+        store.commit('setAuthValue', true)
+      })
+      router.replace('/hello')
     }
+  } else {
+    authErrText.value = "Ошибка авторизации"
+    authErr.value = true
+  }
 }
 
 </script>
 
 <style scoped>
-    .main-container{
-        background: linear-gradient(180deg, darkmagenta 0%, rgba(80, 0, 250, 0.93) 100%);
-        min-height: 100vh;
-    }
+.main-container {
+  background: linear-gradient(180deg, darkmagenta 0%, rgba(80, 0, 250, 0.93) 100%);
+  min-height: 100vh;
+}
 
-    .container{
-        min-height: 100vh;
-    }
-    .wrapper-container {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        min-height: 100vh;
-    }
-  .login-container{
-    display: flex;
-    padding-left: 5px;
-    padding-right: 5px;
-    flex-direction: column;
-    width: calc(100vw/100*15);
-    height: calc(100vh/100*55);
-    align-items: center;
-    justify-content: center;
-    background: linear-gradient(180deg, rgb(220, 224, 222) 0%, rgba(142, 145, 144, 0.93) 100%);
-    margin-top: 25px;
-    border-radius: 1rem;
-    box-shadow: 0px 0px 5px 5px rgba(0, 0, 0, 0.5)
-    
-  }
+.container {
+  min-height: 100vh;
+}
 
-  .name{
-    height: 30px;
-    width: 100%;
-    border-radius: 0.5rem;
-    border: 0;
-  }
+.wrapper-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+}
 
-  .name:focus{
-    color: #212529;
-    background-color: #fff;
-    border-color: #bdbdbd;
-    outline: 0;
-    box-shadow: 0 0 0 0.1rem rgba(71, 71, 71, 0.25);
-  }
+.name {
+  height: 30px;
+  width: 100%;
+  border-radius: 0.5rem;
+  border: 0;
+}
 
-  .password{
-    height: 30px;
-    border-radius: 0.5rem;
-    border: 0;
-    width: 100%;
-  }
+.name:focus {
+  color: #212529;
+  background-color: #fff;
+  border-color: #bdbdbd;
+  outline: 0;
+  box-shadow: 0 0 0 0.1rem rgba(71, 71, 71, 0.25);
+}
 
-  .password:focus{
-    color: #212529;
-    background-color: #fff;
-    border-color: #bdbdbd;
-    outline: 0;
-    box-shadow: 0 0 0 0.1rem rgba(71, 71, 71, 0.25);
-  }
+.password {
+  height: 30px;
+  border-radius: 0.5rem;
+  border: 0;
+  width: 100%;
+}
 
-  .btn-auth{
-    margin-top: 10px;
-    max-width: 100%;
-    border-radius: 1rem;
-    border: 0;
-    background-color: rgb(255, 255, 255);
-    color: rgb(0, 0, 0);
-  }
+.password:focus {
+  color: #212529;
+  background-color: #fff;
+  border-color: #bdbdbd;
+  outline: 0;
+  box-shadow: 0 0 0 0.1rem rgba(71, 71, 71, 0.25);
+}
 
-  .btn-auth:hover{
-    background-color: rgb(30, 20, 98);
-    color: rgb(255, 255, 255);
-    transition: all 0.2s;
-    cursor: pointer;
-  }
+.btn-auth:hover {
+  background-color: rgb(30, 20, 98);
+  color: rgb(255, 255, 255);
+  transition: all 0.2s;
+  cursor: pointer;
+}
 
-  .text-reg{
-    font-size: calc(100vh/60);
-  }
+.text-reg {
+  font-size: calc(100vh/60);
+}
 
-  .link-reg{
-    color: rgb(30, 20, 98);
-  }
-  
-  .form-control.invalid input{
-    border: 2px solid;
-    border-color: rgb(216, 97, 0);
-  }
+.link-reg {
+  color: rgb(30, 20, 98);
+}
 
-  .errors{
-    color: rgb(28, 28, 28);
-    font-size: 11px;
-    margin-left: 3px;
-  }
+.form-control.invalid input {
+  border: 2px solid;
+  border-color: rgb(216, 97, 0);
+}
 
-  .btn-auth:disabled{
-    cursor: default;
-    color: #212529;
-    background-color: #848688;
-  }
+.errors {
+  color: rgb(28, 28, 28);
+  font-size: 11px;
+  margin-left: 3px;
+}
 
+.btn-auth:disabled {
+  cursor: default;
+  color: #212529;
+  background-color: #848688;
+}
 </style>
