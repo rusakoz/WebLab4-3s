c721e0d6c3b8437df1683bd5a7339acbb94f6cfe
Ruslan <julyrak@gmail.com>
2024-08-05 22:13:14 +0300

    Final unit tests

--- src/test/java/org/lab4/wed/weblab4/CheckHitTest.java
+++ src/test/java/org/lab4/wed/weblab4/CheckHitTest.java
@@ -1,7 +1,5 @@
 package org.lab4.wed.weblab4;
 
-
-
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -17,114 +15,109 @@ class CheckHitTest {
         assertTrue(CheckHit.checkHit(-1, -1, 2));
     }
 
-    // @Test
-    // public void hitCircleLowBorder() {
-    //     assertTrue(CheckHit.checkHit(-1, -1, 2));
-    // }
+    @Test
+    public void hitCircleBottomBorder() {
+        assertTrue(CheckHit.checkHit(0, -2, 2));
+    }
 
-    // @Test
-    // public void hitCircleArchBorder() {
-    //     assertTrue(CheckHit.checkHit(-1, -1, 2));
-    // }
+    @Test
+    public void hitCircleTopBorder() {
+        assertTrue(CheckHit.checkHit(-2, 0, 2));
+    }
+
+    @Test
+    public void hitCircleArchBorder() {
+        assertTrue(CheckHit.checkHit(-1.58, -1.22, 2));
+    }
 
     @Test
     public void missCircle() {
         assertFalse(CheckHit.checkHit(-2, -2, 2));
     }
 
-    // @Test
-    // public void missCircleLowBorder() {
-    //     assertFalse(CheckHit.checkHit(-2, -2, 2));
-    // }
+    @Test
+    public void missCircleBottomBorder() {
+        assertFalse(CheckHit.checkHit(0, -2.01, 2));
+    }
 
-    // @Test
-    // public void missCircleArchBorder() {
-    //     assertFalse(CheckHit.checkHit(-2, -2, 2));
-    // }
+    @Test
+    public void missCircleTopBorder() {
+        assertFalse(CheckHit.checkHit(-2.01, 0, 2));
+    }
 
-    // @Test
-    // public void hitTriangle() {
-    //     assertTrue(CheckHit.checkHit(-1, -1, 2));
-    // }
+    @Test
+    public void missCircleArchBorder() {
+        assertFalse(CheckHit.checkHit(-1.58, -1.24, 2));
+    }
 
-    // @Test
-    // public void hitTriangleLowBorder() {
-    //     assertTrue(CheckHit.checkHit(-1, -1, 2));
-    // }
+    @Test
+    public void hitTriangle() {
+        assertTrue(CheckHit.checkHit(1, -1, 3));
+    }
 
-    // @Test
-    // public void hitTriangleTopBorder() {
-    //     assertTrue(CheckHit.checkHit(-1, -1, 2));
-    // }
+    @Test
+    public void hitTriangleLowBorder() {
+        assertTrue(CheckHit.checkHit(1, -3, 3));
+    }
 
-    // @Test
-    // public void hitTriangleHBorder() {
-    //     assertTrue(CheckHit.checkHit(-1, -1, 2));
-    // }
+    @Test
+    public void hitTriangleTopBorder() {
+        assertTrue(CheckHit.checkHit(1, 0, 3));
+    }
 
-    // @Test
-    // public void missTriangle() {
-    //     assertFalse(CheckHit.checkHit(-2, -2, 2));
-    // }
+    @Test
+    public void hitTriangleRightBorder() {
+        assertTrue(CheckHit.checkHit(1.5, -1.5, 3));
+    }
 
-    // @Test
-    // public void missTriangleHBorder() {
-    //     assertFalse(CheckHit.checkHit(-2, -2, 2));
-    // }
+    @Test
+    public void missTriangle() {
+        assertFalse(CheckHit.checkHit(2, 2, 3));
+    }
 
-    // @Test
-    // public void missTriangleTopBorder() {
-    //     assertFalse(CheckHit.checkHit(-2, -2, 2));
-    // }
+    @Test
+    public void missTriangleRightBorder() {
+        assertFalse(CheckHit.checkHit(1.501, -1.5, 3));
+    }
 
-    // @Test
-    // public void hitRectangle() {
-    //     assertTrue(CheckHit.checkHit(-1, -1, 2));
-    // }
+    @Test
+    public void hitRectangle() {
+        assertTrue(CheckHit.checkHit(0.9, 0.9, 3));
+    }
 
-    // @Test
-    // public void hitRectangleLowBorder() {
-    //     assertTrue(CheckHit.checkHit(-1, -1, 2));
-    // }
+    @Test
+    public void hitRectangleLowRightBorder() {
+        assertTrue(CheckHit.checkHit(3, 0, 3));
+    }
 
-    // @Test
-    // public void hitRectangleLeftBorder() {
-    //     assertTrue(CheckHit.checkHit(-1, -1, 2));
-    // }
+    @Test
+    public void hitRectangleLeftTopBorder() {
+        assertTrue(CheckHit.checkHit(0, 3, 3));
+    }
 
-    // @Test
-    // public void hitRectangleRightBorder() {
-    //     assertTrue(CheckHit.checkHit(-1, -1, 2));
-    // }
+    @Test
+    public void hitRectangleRightBorder() {
+        assertTrue(CheckHit.checkHit(1.5, 1.5, 3));
+    }
 
-    // @Test
-    // public void missRectangle() {
-    //     assertFalse(CheckHit.checkHit(-2, -2, 2));
-    // }
+    @Test
+    public void missRectangle() {
+        assertFalse(CheckHit.checkHit(1.7, 1.7, 3));
+    }
 
-    // @Test
-    // public void missRectangleLowBorder() {
-    //     assertFalse(CheckHit.checkHit(-2, -2, 2));
-    // }
+    @Test
+    public void missRectangleLowTopBorder() {
+        assertFalse(CheckHit.checkHit(3, -0.001, 3));
+    }
 
-    // @Test
-    // public void missRectangleLeftBorder() {
-    //     assertFalse(CheckHit.checkHit(-2, -2, 2));
-    // }
+    @Test
+    public void missRectangleLeftTopBorder() {
+        assertFalse(CheckHit.checkHit(-0.001, 3, 3));
+    }
 
-    // @Test
-    // public void missRectangleRightBorder() {
-    //     assertFalse(CheckHit.checkHit(-2, -2, 2));
-    // }
-
-    // @Test
-    // public void hitEmptySector() {
-    //     assertFalse(CheckHit.checkHit(-2, -2, 2));
-    // }
-
-    // @Test
-    // public void hitZero() {
-    //     assertTrue(CheckHit.checkHit(-1, -1, 2));
-    // }
-
+    @Test
+    public void missRectangleRightBorder() {
+        assertFalse(CheckHit.checkHit(1.5001, 1.5001, 3));
+    }
+    
 }
